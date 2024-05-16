74a238c8af49cd627150dd67b066781b6cbca4bc
przemyslawkrepski <krepski80@gmail.com>
2024-05-16 21:34:11 +0200

    kick off with datastructures part

---
+++ src/data_structures/Stack.js
@@ -0,0 +1,14 @@
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+    popStack(){
+
+    }
+    pushStack(value){
+        this.data.push(value)
+    }
+    isEmpty(){
+
+    }
+}
