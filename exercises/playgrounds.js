f3ae8da002133f21ab24880b9f95b4f581a700c1
Przemek <krepski80@gmail.com>
2024-06-18 17:13:39 +0200

    sss

--- exercises/playgrounds.js
+++ exercises/playgrounds.js
@@ -1,101 +1,30 @@
-//Promises
-// function boilWater() {
-//     console.log('Boil Water..');
-//     return new Promise(resolve => setTimeout(resolve, 1000));
-//
-// }
-//
-// function makeATea() {
-//     console.log('Preparing A Tea!..');
-//     return new Promise(resolve => setTimeout(resolve, 2000));
-//
-// }
-// function waitForATemp() {
-//     console.log('Wait For ATemp...');
-//     return new Promise(resolve => setTimeout(resolve, 3000));
-//
-// }
-//
-// boilWater()
-//     .then(() => {
-//         console.log('Water boiled');
-//         return makeATea();
-//     })
-//     .then(() => {
-//         console.log('Tea is made');
-//         return waitForATemp();
-//     })
-//     .then(() => {
-//         console.log('Temperatue is perfect - time to drink');
-//     })
-
-//Async Await
-
-
-function boilWater() {
-    console.log('Boil Water..');
-    return new Promise(resolve => setTimeout(resolve, 1000));
-
+function createCounter() {
+    let counter = 0;
+    return function () {
+        return ++counter;
+    };
 }
 
-function makeATea() {
-    console.log('Preparing A Tea!..');
-    return new Promise(resolve => setTimeout(resolve, 2000));
-
+function filterArray(arr, clb) {
+    const arr2 = [];
+    arr.forEach(a => {
+        if (clb(a)) arr2.push(a);
+    });
+    return arr2
 }
 
-function waitForATemp() {
-    console.log('Wait For ATemp...');
-    return new Promise(resolve => setTimeout(resolve, 3000));
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-}
+const arr2 = filterArray(array, (a) => a % 2 === 0);
 
-// (async () => {
-//     await boilWater();
-//     console.log('Water boiled');
-//     await makeATea();
-//     console.log('Tea is made');
-//     await waitForATemp();
-//     console.log('Temperature is perfect - time to drink');
-// })();
+console.log(arr2);
+const counter = createCounter();
 
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
 
 
-function doMyjob(hours) {
-    return new Promise((resolve, reject) => {
-        if (hours > 6) {
-            reject(new Error('zadlugie godziny pracy'));
-        } else {
-            setTimeout(() => {
-                resolve();
-            }, hours * 1000);
-        }
-    })
-
-}
-
-function pay() {
-    return new Promise(resolve => resolve())
-}
-
-//
-// doMyjob(3)
-//     .then(() => {
-//         console.log("all is good, ");
-//         return pay()
-//     })
-//     .then(() => console.log('wyplata poszla'))
-//     .catch(error => console.log("Blad!", error));
-
-
-// (async () => {
-//     try {
-//         await doMyjob(5);
-//         console.log('Done!');
-//         await pay();
-//         console.log('Wyplata poszla!');
-//
-//     } catch (e) {
-//         console.log("Blad! ", e);
-//     }
-// })();
